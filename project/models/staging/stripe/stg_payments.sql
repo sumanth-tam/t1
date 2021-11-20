dbb45fc9dee3a1987d2e23381332c52e99fab61b
sumanth@tamgroup.com <sumanth@tamgroup.com>
2021-11-20 16:24:03 +0000

    adding tests

--- project/models/staging/stripe/stg_payments.sql
+++ project/models/staging/stripe/stg_payments.sql
@@ -1,14 +1,11 @@
-with payments as (
+select
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
 
-    select
-        id as payment_id,
-        orderid,
-        paymentmethod,
-        status as payment_status,
-        amount,
-        created as payment_date
+    -- amount is stored in cents, convert it to dollars
+    amount / 100 as amount,
+    created as created_at
 
-    from BOOTCAMP_DB.stripe.payment
-)
-
-select * from payments
+from BOOTCAMP_DB.stripe.payment
